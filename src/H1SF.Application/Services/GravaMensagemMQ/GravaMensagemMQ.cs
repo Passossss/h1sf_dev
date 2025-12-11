2c815d07df99ba0e1d4ddd5ba509078cb5d220c3
Bruno DAlessio <brunojava@hotmail.com>
2025-12-11 13:30:11 -0300

    555-00-GRAVA-MENSAGEM-MQ - OK Mock 900-00-REGISTRA-ERRO-TRNS - OK Mock

---
+++ src/H1SF.Application/Services/GravaMensagemMQ/GravaMensagemMQ.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1SF.Application.Services
+{
+    public class GravaMensagemMQ : IGravaMensagemMQ
+    {
+        //555-00-GRAVA-MENSAGEM-MQ
+        //Mock
+        //Mockado por verificar valor em uma condição utilizando CICS
+        public void Executar()
+        {
+            // Lógica do serviço GravaMensagemMQ
+        }
+    }
+}
