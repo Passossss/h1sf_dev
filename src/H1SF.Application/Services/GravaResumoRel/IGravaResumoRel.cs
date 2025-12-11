5520d87746ace9f91a5f4b019a47c934c9786b0e
Bruno DAlessio <brunojava@hotmail.com>
2025-12-11 11:05:32 -0300

    550-00-GRAVA-RESUMO-REL

---
+++ src/H1SF.Application/Services/GravaResumoRel/IGravaResumoRel.cs
@@ -0,0 +1,25 @@
+﻿namespace H1SF.Application.Services
+{
+    public interface IGravaResumoRel
+    {
+        void Executar(
+            System.Data.IDbConnection connection,
+            string st0001CdRegrFtrm,
+            string cb0004CdTPrd,
+            string cb0001RreIdPrcpPtdLit,
+            ref string ws31ChvComandoPjl,
+            ref string cb0001RreCdSqnPjl,
+            string cb0001RreCdStm,
+            string cb0001RreDtcGrc,
+            string cb0001RreIdPrcp,
+            string cb0001RreCdSqnDct,
+            string cb0001RreIdAuxImps1,
+            string cb0001RreIdAuxImps2,
+            string cb0001RreIdAuxImps3,
+            string cb0001RreIdAuxImps4,
+            string cb0001RreIdAuxImps5,
+            string wi01RreIdAuxImps5,
+            string cb0001RreIdRel,
+            string cb0001RreIdImpr);
+    }
+}
